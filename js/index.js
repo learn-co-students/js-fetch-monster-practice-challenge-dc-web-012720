ef4d0c15ee5fc64bcda8004db10506c620ed73c3
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-15 11:55:30 -0400

    Completed

--- js/index.js
+++ js/index.js
@@ -2,9 +2,68 @@ document.addEventListener("DOMContentLoaded", () => {
     // console.log("woo");
     fetchMonsters();
     clickForBtn();
+    createForm();
 })
 
 let page_number = 1;
+
+function createForm() {
+    let targetDiv = document.getElementById('create-monster')
+
+    let formCreate = document.createElement('form')
+
+    // Creating Inputs and Buttons
+        let inputName = document.createElement('input')
+        let inputAge = document.createElement('input')
+        let inputBio = document.createElement('input')
+        let formButton = document.createElement('button')
+
+    // Assign Id's and Whatnot
+        formCreate.id = 'monster-form'
+        inputName.id = 'form-name'
+        inputAge.id = 'form-age'
+        inputBio.id = 'form-bio'
+        formButton.innerHTML = 'Create'
+
+    // Give PlaceHolders
+        inputName.placeholder = 'Name: '
+        inputAge.placeholder = "Age: "
+        inputBio.placeholder = "Description: "
+
+    // Add event to button
+        formButton.addEventListener('click', processForm)
+
+    // Append Elements
+        formCreate.append(inputName, inputAge, inputBio, formButton)
+        targetDiv.append(formCreate)
+
+}
+
+function processForm(event) {
+    let theForm = document.querySelector('#monster-form')
+    event.preventDefault()
+    theForm.reset()
+    let name = document.getElementById('form-name').value
+    let age = document.getElementById('form-age').value 
+    let bio = document.getElementById('form-bio').value
+
+
+    fetch('http://localhost:3000/monsters', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "age": age,
+            "description": bio
+    })
+    })
+    // .then(res => res.json())
+    // .then(monst => renderMonsters(monst))
+    
+}
 
 function btnResponse(event) {
     document.querySelector('#monster-container').innerHTML = ""
